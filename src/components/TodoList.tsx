32336e40e28001db574eeff6b4e9a6d8b6054107
Patriks Briedis <patriks.bried@gmail.com>
2023-10-09 21:05:59 +0100

    Color style change

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,7 +7,7 @@ function TodoList({ todos, handleDelete, handleEdit }: TodoListType) {
     <div>
       {todos.map((todo) => (
         <div className="mb-4 flex w-96 select-none flex-row" key={todo.id}>
-          <div className="flex h-16 w-full items-center justify-between rounded-lg bg-purple-400 p-3 text-white">
+          <div className="flex h-16 w-full items-center justify-between rounded-lg bg-slate-500 p-3 text-white transition duration-200 hover:bg-slate-600">
             <p className="overflow-hidden break-words">{todo.content}</p>
             <div className="flex cursor-pointer items-center gap-2 text-3xl">
               <AiFillEdit onClick={() => handleEdit(todo.id)} />
