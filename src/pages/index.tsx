71197a0bd783b09f84ebdecf91ce55aab13bda90
Patriks B <60120715+Wex2k@users.noreply.github.com>
2025-05-10 23:20:37 +0100

    fix(todos): extract TodoList from AddTodo component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { ActionBar } from "@/components/ActionBar";
 import { useContext } from "react";
 import { TodoContext } from "@/contexts/TodoContext/TodoContext";
 import AddTodo from "@/components/AddTodo";
+import TodoList from "@/components/TodoList";
 
 export default function Home() {
   const { loaded } = useContext(TodoContext)!;
@@ -17,7 +18,11 @@ export default function Home() {
       <main className="min-h-screen scroll-smooth bg-black/90 p-3 font-sans text-white selection:bg-white/20 md:p-6">
         {/* Main Content */}
         <div className="flex justify-center">
-          <AddTodo />
+          <div className="flex flex-col gap-9">
+            <AddTodo />
+
+            {loaded && <TodoList />}
+          </div>
         </div>
 
         <ActionBar loaded={loaded} />
