a00092cb225a0ac2593a4b22701d931229ffa438
Patriks B <60120715+Wex2k@users.noreply.github.com>
2025-07-13 11:07:13 +0100

    feat(themes): start adding theming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,22 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "@/styles/globals.css";
+
 import TodoProvider from "@/contexts/TodoContext/TodoContext";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <TodoProvider>
-      <Component {...pageProps} />
-    </TodoProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <TodoProvider>
+        <Component {...pageProps} />
+      </TodoProvider>
+    </ThemeProvider>
   );
 };
 
