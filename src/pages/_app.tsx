97bec9e42bff1ac31282e4f08425453194eb68b2
Patriks B <60120715+Wex2k@users.noreply.github.com>
2026-03-07 14:36:22 +0000

    misc: update shadcn registry components to latest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import "@/styles/globals.css";
 import { TodoProvider } from "@/contexts/TodoContext/TodoContext";
 import { ThemeProvider } from "@/providers/ThemeProvider";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
@@ -12,9 +13,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       enableSystem
       disableTransitionOnChange
     >
-      <TodoProvider>
-        <Component {...pageProps} />
-      </TodoProvider>
+      <TooltipProvider>
+        <TodoProvider>
+          <Component {...pageProps} />
+        </TodoProvider>
+      </TooltipProvider>
     </ThemeProvider>
   );
 };
