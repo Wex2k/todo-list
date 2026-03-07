3c7f590bc3ad4d8a2f4e397856597f6220d9131b
Patriks B <60120715+Wex2k@users.noreply.github.com>
2026-03-07 15:14:33 +0000

    misc: cleanup eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,21 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default defineConfig([
-  globalIgnores(["**/node_modules/", "**/.next/", "src/components/ui"]),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
   {
-    extends: compat.extends(
-      "next/core-web-vitals",
-      "plugin:@typescript-eslint/recommended-type-checked",
-      "plugin:@typescript-eslint/stylistic-type-checked",
-    ),
-
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "script",
-
-      parserOptions: {
-        project: true,
-      },
-    },
+    files: ["**/*.{ts,tsx}"],
   },
 ]);
+
+export default eslintConfig;
